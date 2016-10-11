8d8b57ea5b88737bbf00580c8a6a169ca9fdc9bd
DAM2 <pablo160896@gmail.com>
2016-10-11 17:01:21 +0200

    Commit 2

--- app/src/main/java/com/example/dam2/fragmenttabhost/MainActivity.java
+++ app/src/main/java/com/example/dam2/fragmenttabhost/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.dam2.fragmenttabhost;
 
+import android.content.res.Resources;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
+    private FragmentTabHost tabHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tabHost =
+                (FragmentTabHost) findViewById(android.R.id.tabhost);
+        tabHost.setup(this, getSupportFragmentManager(),android.R.id.tabcontent);
+
+        Resources res = getResources();
+
+        
     }
 }
